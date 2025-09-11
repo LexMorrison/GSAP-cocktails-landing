c336a507806bedf4fd81a9b7c3a1c87416bd9d31
LexMorrison <borrlex11@gmail.com>
2025-09-11 13:32:35 +0200

    finished hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,14 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { SplitText } from "gsap/all";
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
   useGSAP(() => {
     const heroSplit = new SplitText(".title", { type: "chars, words" }); //split by letters
     const paragraphSplit = new SplitText(".subtitle", { type: "lines" }); //split line by line
@@ -38,6 +44,41 @@ const Hero = () => {
       })
       .to(".right-leaf", { y: 200 }, 0)
       .to(".left-leaf", { y: -200 }, 0);
+
+    // set scrollTrigger values depending on screen size
+    const startValue = isMobile ? "top 50%" : "center 60%";
+    const endValue = isMobile ? "120% top" : "bottom top";
+
+    // create a timeline that pins and scrubs the video
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "video",
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      },
+    });
+
+    // --- VIDEO PART ---
+    const video = videoRef.current;
+    if (video) {
+      // function that plays the video timeline to the end
+      const playToEnd = () =>
+        tl.to(video, {
+          // animate the currentTime of the video until the end
+          currentTime: video.duration,
+          duration: video.duration || 1, // fallback if duration is 0
+        });
+
+      // if metadata (duration) is already loaded, run immediately
+      if (video.readyState >= 1) {
+        playToEnd();
+      } else {
+        // otherwise wait until video metadata is loaded
+        video.onloadedmetadata = playToEnd;
+      }
+    }
   }, []);
   return (
     <>
@@ -72,6 +113,15 @@ const Hero = () => {
           </div>
         </div>
       </section>
+      <div className="video absolute inset-0">
+        <video
+          ref={videoRef}
+          src="/videos/output.mp4"
+          muted
+          playsInline
+          preload="auto"
+        />
+      </div>
     </>
   );
 };
